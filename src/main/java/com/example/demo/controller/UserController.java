3fd787d2a1b6628661d39c421a1a0ce42279349a
longhoang130704 <long.hoang130704@hcmut.edu.vn>
2025-05-22 10:33:32 +0700

    [CHORE] commit first

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -14,4 +14,6 @@ public class UserController {
     public ResponseEntity<String> createUser(@Valid @RequestBody UserCreationRequest request) {
         return ResponseEntity.ok("User created successfully!");
     }
+    // abc abc 
+    // thay doi code de conflict
 }
